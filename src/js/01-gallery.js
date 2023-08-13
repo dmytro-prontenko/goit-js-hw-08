1955cdcfbebf2b8e2f5b311ab1ef945b13065cc3
Dmytro Prontenko <dptontenko@gmail.com>
2023-08-13 12:57:42 +0200

    #1 done

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -2,4 +2,55 @@
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
-console.log(galleryItems);
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+const refs = {
+  gallery: document.querySelector('.gallery'),
+};
+
+function makeGalleryListMarkup(images) {
+  return images
+    .map(
+      ({ preview, original, description }) =>
+        `<li class="gallery__item">
+          <a class="gallery__link" href="${original}">
+              <img
+              class="gallery__image"
+              src="${preview}"
+              alt="${description}" />
+          </a>
+        </li>`
+    )
+    .join('');
+}
+
+refs.gallery.insertAdjacentHTML(
+  'beforeend',
+  makeGalleryListMarkup(galleryItems)
+);
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  /* options */
+  captionsData: 'alt',
+  captionsDelay: 300,
+  showCounter: false,
+  animationSpeed: 300,
+  download: 'Download this image',
+  fadeSpeed: 300,
+  animationSlide: false,
+});
+
+lightbox.on('shown.simplelightbox', function () {
+  console.log('Галерею завантажено');
+});
+lightbox.on('closed.simplelightbox', function () {
+  console.log('Галерею закрито');
+});
+
+lightbox.on('nextDone.simplelightbox', function () {
+  console.log('Завантажено наступне зображення');
+});
+lightbox.on('prevDone.simplelightbox', function () {
+  console.log('Завантажено попереднє зображення');
+});
