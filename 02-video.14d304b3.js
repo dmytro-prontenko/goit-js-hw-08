81a93ca27690ebf2a870f231ae8066611f4896b2
dmytro-prontenko <136347904+dmytro-prontenko@users.noreply.github.com>
2023-08-14 20:28:22 +0000

    Deploying to gh-pages from @ dmytro-prontenko/goit-js-hw-08@ce3ba47559e185b8719045644a66ac0d96acd548 🚀

--- 02-video.14d304b3.js
+++
@@ -1,2 +0,0 @@
-localStorage.setItem("a",JSON.stringify([1,2,3,4,5])),console.log(localStorage.getItem("a"));
-//# sourceMappingURL=02-video.14d304b3.js.map
--- 02-video.58a9e9b7.js
+++
@@ -1,2 +0,0 @@
-localStorage.setItem("a",JSON.stringify([1,2,3,4,5])),console.log(localStorage.getItem("a"));
-//# sourceMappingURL=02-video.58a9e9b7.js.map
