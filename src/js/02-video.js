0a8215a96aae974027a6fe9f675b98ac48f7da96
Dmytro Prontenko <dptontenko@gmail.com>
2023-08-14 20:12:24 +0200

    #3 done

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,7 @@
+const obj = {
+  a: 10,
+  b: 20,
+};
 
+localStorage.setItem('a', JSON.stringify([1, 2, 3, 4, 5]));
+console.log(localStorage.getItem('a'));
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,30 @@
+import throttle from 'lodash.throttle';
 
+const refs = {
+  email: document.querySelector('input'),
+  message: document.querySelector('textarea'),
+  form: document.querySelector('.feedback-form'),
+};
+
+const KEY_FORM = 'feedback-form-state';
+const objToLocalStorage = JSON.parse(localStorage.getItem(KEY_FORM)) ?? {};
+
+if (objToLocalStorage) {
+  refs.email.value = objToLocalStorage.email || '';
+  refs.message.value = objToLocalStorage.message || '';
+}
+
+refs.form.addEventListener('input', throttle(onFormInput, 500));
+refs.form.addEventListener('submit', onFormSubmit);
+
+function onFormInput(event) {
+  objToLocalStorage[event.target.name] = event.target.value;
+  localStorage.setItem(KEY_FORM, JSON.stringify(objToLocalStorage));
+}
+
+function onFormSubmit(event) {
+  event.preventDefault();
+  console.log(localStorage.getItem(KEY_FORM));
+  event.currentTarget.reset();
+  localStorage.clear();
+}
